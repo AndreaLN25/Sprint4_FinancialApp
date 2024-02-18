c1ce4ce28e99149b969959b80f3f639138a08b16
AndreaLN25 <andrealn14@gmail.com>
2024-02-18 16:29:05 +0100

    Clean Factory

--- database/factories/SharedTransactionModelFactory.php
+++ database/factories/SharedTransactionModelFactory.php
@@ -31,31 +31,11 @@ class SharedTransactionModelFactory extends Factory
 
             $totalAmount = $this->faker->randomFloat(2, 0, 100000);
 
-  /*           dd([
-                'user_id' => UserModel::factory()->create()->id,
-                'transaction_id' => TransactionModel::factory()->create()->id,
-                'amount' => $totalAmount,
-                'user_paid' => UserModel::factory()->create()->full_name,
-                'number_of_participants' => count($participants),
-                'name_of_participants' => implode(', ', $participants),
-                'amount_per_participant' => $totalAmount/$numberOfParticipants,
-                'date' => $this->faker->date,
-                'description' => $this->faker->sentence,
-                'approval_status' => $this->faker->randomElement(['pending', 'approved', 'rejected']),
-                'note' => $this->faker->sentence,
-            ]);
-             */
-
             return [
                 'user_id' => UserModel::factory()->create()->id,
                 'transaction_id' => TransactionModel::factory()->create()->id,
                 'amount' => $totalAmount,
-                //'user_paid' => UserModel::all()->random()->first_name . ' ' . UserModel::all()->random()->last_name,
-                //'user_paid' => UserModel::all()->random()->full_name,
-                //'user_paid' => UserModel::all()->random()->id,
-                //'user_paid' => UserModel::all()->random()->getFullNameAttribute(),
                 'user_paid' => UserModel::factory()->create()->full_name,
-
                 'number_of_participants' => count($participants),
                 'name_of_participants' => implode(', ', $participants),
                 'amount_per_participant' => $totalAmount/$numberOfParticipants,
